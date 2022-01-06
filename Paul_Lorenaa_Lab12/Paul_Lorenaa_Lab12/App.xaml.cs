e0f658cdf2b0598f87db2d75e4967171c6af406d
Lorena Amalia Paul <Paul.Va.Lorena@student.utcluj.ro>
2022-01-06 19:02:03 +0200

    Am facut Laboratorul 12

---
+++ Paul_Lorenaa_Lab12/Paul_Lorenaa_Lab12/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Paul_Lorenaa_Lab12.Data;
+
+namespace Paul_Lorenaa_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
